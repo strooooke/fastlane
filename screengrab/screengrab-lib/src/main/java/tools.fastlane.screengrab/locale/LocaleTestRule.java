37b4f624449adcfbed18216ae3e83a809e87bd7e
Juliane Lehmann <jule.germany@googlemail.com>
2016-05-12 16:59:51 +0200

    Change LocaleTestRule to late-bind the locales
    Allows nesting LocaleTestRule in a RuleChain with an outer rule modifying the instrumentation registry.

--- screengrab/screengrab-lib/src/main/java/tools.fastlane.screengrab/locale/LocaleTestRule.java
+++ screengrab/screengrab-lib/src/main/java/tools.fastlane.screengrab/locale/LocaleTestRule.java
@@ -8,30 +8,21 @@ import java.util.Locale;
 
 public class LocaleTestRule implements TestRule {
 
-    private final Locale testLocale;
-    private final Locale endingLocale;
-
-    public LocaleTestRule() {
-        this(LocaleUtil.getTestLocale(), LocaleUtil.getEndingLocale());
-    }
-
-    public LocaleTestRule(Locale testLocale, Locale endingLocale) {
-        this.testLocale = testLocale;
-        this.endingLocale = endingLocale;
-    }
-
     @Override
-    public Statement apply(final Statement base, Description description) {
+    public Statement apply(Statement base, Description description) {
+        Locale testLocale = LocaleUtil.getTestLocale();
+        Locale endingLocale = LocaleUtil.getEndingLocale();
+
         return new Statement() {
-            @Override
             public void evaluate() throws Throwable {
                 try {
-                    if (testLocale != null) {
+                    if(testLocale != null) {
                         LocaleUtil.changeDeviceLocaleTo(testLocale);
                     }
+
                     base.evaluate();
                 } finally {
-                    if (endingLocale != null) {
+                    if(endingLocale != null) {
                         LocaleUtil.changeDeviceLocaleTo(endingLocale);
                     }
                 }
